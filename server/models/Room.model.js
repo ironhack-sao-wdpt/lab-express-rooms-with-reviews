02c88bfb5bccd9f82e79edba5b66a8c85442746d
Camila Solér <msoler.camila@gmail.com>
2022-05-23 21:43:12 -0300

    begin to work at review

--- server/models/Room.model.js
+++ server/models/Room.model.js
@@ -4,7 +4,7 @@ const RoomSchema = new Schema({
   name: { type: String },
   description: { type: String },
   imageUrl: { type: String },
-  reviews: [], // we will update this field a bit later when we create review model
+  reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
 });
 
 module.exports = model("Room", RoomSchema);
